e75beda2b83ff087d38fdc4ec892acabc7155624
deni zakya <denizakya@gmail.com>
2022-06-11 23:37:04 +0200

    Add fibo

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,27 @@ func routes(_ app: Application) throws {
     app.get("hallo") { req -> String in
         return "Hallo!!"
     }
+
+    app.get("fibo") { req -> String in
+        fibo()
+    }
+
+    func fibo(_ i: Int = 100) -> String {
+        var result = ""
+        var m,n = 1
+        for num in 1...i {
+            if num == 1 { 
+                result += " \(m)" 
+            } else if num == 2 {
+                result += " \(n)"
+            } else {
+                let o = m + n
+                result += " \(o)"
+                m = n
+                n = 0
+            }
+        }
+
+        return result
+    }
 }

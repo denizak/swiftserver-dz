de0de15f05ca83507d0ba6086852c34b1ecb9d62
deni zakya <denizakya@gmail.com>
2022-06-12 13:17:11 +0200

    add max index for fibo function

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,15 +11,16 @@ func routes(_ app: Application) throws {
 
     app.get("fibo", ":index") { req -> String in
         let index = req.parameters.get("index")!
-        let indexInt = Int(index) ?? 1
+        let indexInt = UInt(index) ?? 1
         return fibo(until: indexInt)
     }
 
-    func fibo(until index: Int) -> String {
+    func fibo(until index: UInt) -> String {
+        let allowedIndex = min(index, 90)
         var results: [Int] = []
         var m = 1
         var n = 1
-        for num in 1...index {
+        for num in 1...allowedIndex {
             if num == 1 { 
                 results.append(m)
             } else if num == 2 {
